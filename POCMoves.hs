f6e64dbf56f9e5019e7f28e1cdb83417ca939c31
Oscar Kalldal <oscar@oscar-MacBookPro.(none)>
2014-02-17 16:32:22 +0100

    Added Move type

--- POCMoves.hs
+++ POCMoves.hs
@@ -4,6 +4,8 @@ import Print
 import Data.Array
 
 data Direction = Up | Down deriving (Eq)
+type Move = [Coord]
+
 
 revInRng :: (Int, Int) -> Int -> Int
 revInRng (a, b) i = a + b - i
@@ -11,21 +13,21 @@ revInRng (a, b) i = a + b - i
 revChain :: AChain -> AChain
 revChain ch = ixmap (bounds ch) (revInRng (bounds ch)) ch
 
-doMove :: AChain -> Int -> Direction -> Chain -> Chain
+doMove :: AChain -> Int -> Direction -> Move -> Chain
 doMove ch i Up move = reverse $ doMove (revChain ch) (revInRng (bounds ch) i) Down move
 doMove ch i Down move = let ch' = elems ch in take (i - length move) ch' ++ reverse move ++ drop i ch'
 
 --allPulls :: Chain -> [Chain]
 --allPulls ch = concat $ map (\
 
-pullMoves :: AChain -> Int -> Direction -> [Chain]
+pullMoves :: AChain -> Int -> Direction -> [Move]
 pullMoves ch i dir | not iValid = []
 		   | dir == Up = pullMoves (revChain ch) (revInRng (bounds ch) i) Down
 		   | dir == Down = map (pull ch i) (valCLs ch i)
 	where
 		iValid = let (a,b) = bounds ch in i>a && i<b
 
-pull :: AChain -> Int -> (Coord, Coord) -> [Coord]
+pull :: AChain -> Int -> (Coord, Coord) -> Move
 pull ch i (c, l) | ch!(i-1) == c = [l]
 		 | otherwise = l:c:(follow (i-2))
 	where

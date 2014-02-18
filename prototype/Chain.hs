e024ce04a86d4ae63a4f7004cad07c4040e9cbe5
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2014-02-18 13:35:34 +0100

    Changes to prototype

--- prototype/Chain.hs
+++ prototype/Chain.hs
@@ -1,18 +1,44 @@
+{-# LANGUAGE GADTs #-}
+
 module Chain where
+
+import qualified Data.Vector as V
 
 class (Eq a, Ord a) => Coord a where
     adj :: a -> a -> Bool
     dadj :: a -> a -> Bool
     neighbors :: a -> a -> [(a,a)]
 
+data Chain a where
+    Chain :: (Coord a) => 
+        (V.Vector a) ->        -- A list of our coordinates in chain order
+        Maybe (V.Vector a) ->  -- A sorted list of our coords
+        Chain a
 
-class Chain a where
-    cReverse :: a -> a
-    (!) :: Coord b => Int -> a -> b
-    cHead ::  Coord b => a -> b
-    --cTake :: a -> a
-    --cDrop :: a -> a
-    cTail :: a -> a
+toVector :: Chain a -> Vector a
+toVector (Chain v _) = v
+
+-- Public functions
+cReverse :: Chain a -> Chain a
+cReverse v = Chain (V.reverse . toVector v) Nothing
+
+(!) :: Int -> Chain a -> a
+(!) = undefined
+
+cHead :: Chain a -> a
+cHead = undefined
+
+cTake :: Int -> Chain a -> Chain a
+cTake = undefined
+
+cDrop :: Int -> Chain a -> Chain a
+cDrop = undefined
+
+cTail :: Chain a -> Chain a
+cTail = undefined
+
+cEmpty :: Chain a -> a -> Bool
+cEmpty = undefined
 
 data Coord2d = Coord2d (Int, Int) deriving (Ord, Eq)
 
@@ -21,10 +47,3 @@ instance Coord Coord2d where
     dadj _ _ = True
     neighbors a b = [(a,b)]
 
-data Coord a => LChain a = LChain {
-    toList :: [a]
-    }
-
-instance Coord a => Chain (LChain a) where
-    cReverse = LChain . reverse . toList
-    (!) el = (el!!) . toList

d3455377f17e54f24879baed3ab6f8bf737d4f4b
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2014-02-20 16:58:44 +0100

    Adds cContains in log n time

--- Chain.hs
+++ Chain.hs
@@ -5,6 +5,7 @@ module Chain (
              , cReverse
              , cEmpty
              , cLength
+             , cContains
              , (!)
              , replace
              ) where
@@ -13,15 +14,23 @@ import qualified Data.Vector as V
 import qualified Data.Set as S
 import Coord
 
+newtype Memb a = M (a, Int)
+
+instance (Ord a) => Ord (Memb a) where
+    compare (M (a, _)) (M (b, _)) = compare a b
+
+instance Eq a => Eq (Memb a) where
+    (==) (M (a, _)) (M (b, _)) = a == b 
+
 data Chain a = Chain (V.Vector a)          -- A list of our coordinates in chain order
-                     (S.Set a)             -- A set of our coords
+                     (S.Set (Memb a))        -- A set of our coords and their indices
                      deriving (Eq)
 
 toVector :: Chain a -> V.Vector a
 toVector (Chain v _) = v
 
 fromVector :: (Ord a) => V.Vector a -> Chain a
-fromVector v = Chain v (S.fromList . V.toList $ v)
+fromVector v = Chain v $ S.fromList $ map M $ zip (V.toList v) [0..]
     
 
 -- Public functions
@@ -45,5 +54,13 @@ replace :: (Ord a) => Chain a -> Int -> [a] -> Chain a
 replace ch i diff = fromVector $ (toVector ch) V.// (zip [i..] diff)
 
 cEmpty :: (Ord a) => Chain a -> a -> Bool
-cEmpty (Chain _ sorted) element = not $ element `S.member` sorted
+cEmpty (Chain _ sorted) element = not $ (M (element,0)) `S.member` sorted
 
+cContains :: (Ord a) => Chain a -> a -> Maybe Int
+cContains ch@(Chain _ sorted) elem = if cEmpty ch elem 
+                                     then Nothing
+                                     else index
+                     where
+                        index = fmap second elem'
+                        elem' = S.lookupGE (M (elem, 0)) sorted
+                        second (M (_,i)) = i

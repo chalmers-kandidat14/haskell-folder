800c13260744fcbb7a663e23867a60675f4bca1d
linusbo <linus.bor@gmail.com>
2014-03-27 11:54:22 +0100

    ...

--- AAConverter.hs
+++ AAConverter.hs
@@ -1,4 +1,6 @@
 
+module AAConverter (translate) where
+
 import System.Environment
 import Control.Monad
 import Data.Char (toUpper)
@@ -7,30 +9,10 @@ type AA = String
 type HP = String
 
 polar :: [(AA, HP)]
-polar = [ ("D","P")
-        , ("E","P")
-        , ("H","P")
-        , ("K","P")
-        , ("N","P")
-        , ("O","P")
-        , ("Q","P")
-        , ("R","P")
-        , ("S","P")
-        , ("W","P")
-        ]
+polar = map (\x -> ([x], "P")) "DEHKNOQRSW"
 
 hydrophobic :: [(AA, HP)]
-hydrophobic = [ ("Y","H")
-              , ("A","H")
-              , ("C","H")
-              , ("F","H")
-              , ("G","H")
-              , ("I","H")
-              , ("L","H")
-              , ("M","H")
-              , ("P","H")
-              , ("V","H")
-              ]
+hydrophobic = map (\x -> ([x], "H")) "YACFGILMPV"
 
 translateAAtoHP :: [AA] -> [(AA, Maybe HP)]
 translateAAtoHP = map (\x -> (x, lookup x aaTable))
@@ -38,11 +20,10 @@ translateAAtoHP = map (\x -> (x, lookup x aaTable))
     aaTable = polar ++ hydrophobic
 
 translate :: [AA] -> [HP]
-translate = f . translateAAtoHP
+translate = map f . translateAAtoHP
   where
-    f ((aa, Nothing):_)  = error ("Could not match the amino acid \"" ++ aa ++ "\" with any entry in the dictionary.")
-    f ((aa, Just x):xs) = x : f xs
-    f []                = []
+    f (aa, Nothing) = error ("Could not match the amino acid \"" ++ aa ++ "\" with any entry in the dictionary.")
+    f (aa, Just x)  = x
 
 main :: IO ()
 main = getArgs >>= putStrLn . unwords . translate . map (map toUpper) . concatMap (concatMap (words) . lines)

094693939249ee62ff57dd7697d202a6b0d4f96b
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2014-04-07 16:43:20 +0200

    Nu fungerar det!!!!!!!!!!!!

--- Similarity.hs
+++ Similarity.hs
@@ -10,22 +10,33 @@ import Chain
 import HPModel
 import Coord
 
-import qualified Data.Vector as V
+import Data.List (intersect)
 import Data.Maybe (mapMaybe, catMaybes)
 
-similarity :: [HPResidue] -> Chain a -> Chain a -> Int
-similarity res ch ch' = undefined
+similarity :: Coord a => [HPResidue] -> Chain a -> Chain a -> Int
+similarity res ch ch' = foldr f 0 $ zip a b 
     where
+        f (xs,ys) acc = acc + (sum (overlap xs ys)) 
+        
+        -- We intersect with the indices of the H residues
+        -- so we only check the connections of the H
+        overlap xs ys = xs `intersect` ys `intersect` validIndices
+       
+        validIndices = map snd $
+                       filter (isHydrophobic . fst) $ 
+                       zip res [0..]
+
+        connections = sum (concat a)
         a = buildConnectionMatrix ch
         b = buildConnectionMatrix ch'
 
 -- Builds a matrix with the the connections for each residue (row)
 -- observe that each connection is only counted once, so the matrix
 -- is not symmetric
-buildConnectionMatrix :: Coord a => Chain a -> V.Vector (V.Vector Int)
-buildConnectionMatrix ch = V.fromList $ map f [0..(n-1)] 
+buildConnectionMatrix :: (Coord a, Num b) => Chain a -> [[b]]
+buildConnectionMatrix ch = map f [0..(n-1)] 
     where
-        f = V.fromList .
+        f = map fromIntegral . 
             mapMaybe (cIndex ch) . 
             validNeighbors ch
         n = cLength ch

dd4004808c0905093650844f06d2dc852251671d
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2014-05-14 02:13:50 +0200

    tochain uses 3d

--- to-graph/Main.hs
+++ to-graph/Main.hs
@@ -4,13 +4,23 @@ import Similarity
 import System.Environment
 
 -- converts an chain-file to a graph
-fileToGraph file = do
+fileToGraphFCC file = do
     content <- readFile file
     let coords = map readFCC $ lines content
     return $ buildGraph (fromList coords)
     
-main = do
-    (file:_) <- getArgs
-    graph <- fileToGraph file
+fileToGraph3d file = do
+    content <- readFile file
+    let coords = map read3D $ lines content
+    return $ buildGraph (fromList coords)
+    
+putGraph graph = do
     putStr $ printGraph graph
+
+main = do
+    args <- getArgs
+    case args of
+        ["3d", file] -> fileToGraph3d file >>= putGraph
+        [file] -> fileToGraphFCC file >>= putGraph
+        otherwise -> putStrLn "Error, wrong arguments"
     
